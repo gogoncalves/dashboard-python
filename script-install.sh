0f95ad8933058f7c165f83365ddd77db23921774
GOGoncalves <gustavo.goncalves@sptech.school>
2022-11-16 22:42:53 -0300

    Atualizacao Script Bash

--- script-install.sh
+++ script-install.sh
@@ -70,48 +70,25 @@ clear
 sudo apt update -y
 clear
 
-PURPLE='0;35'
-NC='\033[0m' 
-VERSAO=11
-
-echo  "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Bom, agora vou verificar aqui se você possui o Java instalado...;"
-sleep 2
-
-if [ $? -eq 0 ]
-then
-    clear
-    echo "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Que ótimo, Você já tem o java instalado!!!"
-    sleep 3
-    clear
-    validacao_mysql
-else
-    echo "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Opa! Não identifiquei nenhuma versão do Java instalado, mas sem problemas, irei resolver isso agora!"
-    echo "$(tput setaf 4)[Bot assistant]:$(tput setaf 7) Confirme para mim se realmente deseja instalar o Java (S/N)?"
-    read inst
-    if [ \"$inst\" == \"S\" ]
-        then
-            echo "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Ok! Você escolheu instalar o Java ;D"
-            echo "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Adicionando o repositório!"
-            sleep 2
-            sudo add-apt-repository ppa:webupd8team/java -y
-            clear
-            echo "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Atualizando! Quase lá."
-            sleep 2
-            clear
-            
-            if [ $VERSAO -eq 11 ]
-                then
-                    echo "$(tput setaf 4)[Bot assistant]:$(tput setaf 7) Preparando para instalar a versão 11 do Java. Confirme a instalação quando solicitado ;D"
-                    sudo apt install default-jre ; apt install openjdk-11-jre-headless; -y
-                    clear
-                    echo "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Java instalado com sucesso!"
-                    validacao_mysql
-                fi
-        else 	
-            echo "$(tput setaf 9)[Bot assistant]:$(tput setaf 7) Você optou por não instalar o Java por enquanto, até a próxima então!"
-            validacao_mysql
-    fi
+echo "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Instalação JAVA versão 11"
+echo "$(tput setaf 4)[Bot assistant]:$(tput setaf 7) Confirme para mim se realmente deseja instalar o Java (S/N)?"
+read inst
+if [ \"$inst\" == \"S\" ]
+    then
+        echo "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Ok! Você escolheu instalar o Java ;D"
+        sleep 2
+        sudo apt install openjdk-11-jre-headless
+        clear
+        echo "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Atualizando! Quase lá."
+        sleep 2
+        clear                
+        echo "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Java instalado com sucesso!"
+        validacao_mysql
+    else 	
+        echo "$(tput setaf 9)[Bot assistant]:$(tput setaf 7) Você optou por não instalar o Java por enquanto, até a próxima então!"
+        validacao_mysql
 fi
+
 }
 
 function validacao_mysql {
@@ -189,7 +166,7 @@ function validacao_pip3 {
         if [ \"$quest3\" == \"S\" ]
             then
                 echo "$(tput setaf 14)[Bot assistant]:$(tput setaf 7) Preparando para instalar o Pip3."
-                echo y | sudo apt install pip3
+                sudo apt install python3-pip -y
                 clear
                 echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Pip3 instalado com sucesso!"
                 validacao_driver
@@ -203,7 +180,6 @@ function validacao_pip3 {
     else
         clear
         echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Ótimo, você já tem o pip3 instalado!!!"
-        sudo apt install python3-pip
         sleep 3
         validacao_driver
     fi
